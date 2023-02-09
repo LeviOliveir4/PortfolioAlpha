384905edf2de2c3013acfb4e54f0be9cb33bef8b
LeviOliveir4 <levideoliveira3002@outlook.com>
2023-02-09 15:02:30 -0300

    Ajustando o About

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -2,9 +2,10 @@
 const AboutContainer = () => {
   return <section className="about-container">
     <h2>Sobre</h2>
-    <p>Sou universitário do Centro Universitário de João Pessoa - UNIPÊ/PB, no curso Análise e Desenvolvimento de Sistemas. Estou me desenvolvendo na área de front-end, sempre estudando e praticando.. Faço parte do projeto de extensão Unipê(Fábrica de Software), onde estou me desenvolvendo muito, e está agregando muito nos meus estudos/projetos pessoais.</p>
-
-    <p>Soft Skills: Proatividade, resiliência, senso de trabalho em equipe, colaboração, responsabilidade e comunicação assertiva.</p>
+    <p>Universitário do Centro Universitário de João Pessoa - UNIPÊ/PB, no curso Análise e Desenvolvimento de Sistemas. Estou me desenvolvendo na área de front-end, sempre estudando e praticando.. Fiz parte do projeto de extensão Unipê(Fábrica de Software), onde me desenvolvi muito, e agregou muito nos meus estudos/projetos pessoais. </p>
+    <strong>Mindset:Crescimento.</strong>
+    <br /> 
+    <p>Soft Skills: Proatividade, Resiliência, Senso de trabalho em equipe, Colaboração, Responsabilidade e Comunicação assertiva.</p>
 
   </section>;
 
