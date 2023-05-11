ddac4ae7110b6e2bf779d47f36568b7f872b268f
LeviOliveir4 <levideoliveira3002@outlook.com>
2023-05-10 21:32:28 -0300

    atualizando o about

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -5,10 +5,10 @@ const AboutContainer = () => {
       <p>
         Universitário do Centro Universitário de João Pessoa- UNIPÊ, no curso
         Análise e Desenvolvimento de Sistemas. Apaixonado por Programação,
-        sempre buscando mais conhecimentos, e formas diferentes para resolução
-        de problemas. Atualmente estou com uma base sólida no desenvolvimento
-        web, e estou buscando mais conhecimento em BackEnd, para agregar na
-        minha carreira como desenvolvedor Full Stack.
+        sempre buscando mais conhecimento, e formas diferentes para resolução de
+        problemas. Atualmente estou com uma base sólida no Desenvolvimento web.
+        Estou me aprimorando em BackEnd, para agregar na minha carreira como
+        desenvolvedor Full Stack.
       </p>
     </section>
   );
